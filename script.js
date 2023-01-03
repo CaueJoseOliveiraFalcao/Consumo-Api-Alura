24b4a73587b09ce363808be0bec5987058037dd9
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-01-03 12:26:13 -0500

    update

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-async function BuscaEndereco(){
+function PegaValor(){
+    let inputCep = document.querySelector('#cep').value
+    BuscaEndereco(inputCep)
+}
+async function BuscaEndereco(inputCep){
     try {
-        var ConsultaCep = await fetch("https://viacep.com.br/ws/01001000/json/")
+        var ConsultaCep = await fetch(`https://viacep.com.br/ws/${inputCep}/json/`)
         var ConsultaCepConvertida = await ConsultaCep.json()
-        console.log(ConsultaCepConvertida)
+        if (ConsultaCepConvertida.erro){
+            throw Error(alert('CEP VALIDO MAS NÃO EXISTENTE'))
+            return
+        }
+        document.querySelector('#endereco').value = ConsultaCepConvertida.logradouro
+        document.querySelector('#complemento').value = ConsultaCepConvertida.complemento
+        document.querySelector('#bairro').value = ConsultaCepConvertida.bairro
+        document.querySelector("#cidade").value = ConsultaCepConvertida.localidade
+        document.querySelector("#uf").value = ConsultaCepConvertida.uf
+        return ConsultaCepConvertida
     }
     catch (erro){
         console.log(erro)
     }
 }
 
-BuscaEndereco()
